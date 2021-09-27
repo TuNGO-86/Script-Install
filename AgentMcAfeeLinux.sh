12fbaf35b60dde871378126ff2a2ad2a6ff1bdb5
ndt1092 <77654266+TuNGO-86@users.noreply.github.com>
2021-09-27 09:25:10 +0700

    Update AgentMcAfeeLinux.sh

--- AgentMcAfeeLinux.sh
+++ AgentMcAfeeLinux.sh
@@ -4,7 +4,7 @@
 
 sudo wget https://github.com/TuNGO-86/script/releases/download/ProductionV1/agentPackagesProduct.zip
 sudo apt install unzip
-sudo unzip agentPackages.zip
+unzip agentPackages.zip
 sudo chmod +x install.sh
 ./install.sh -i
 echo "Installation succeeded!"
